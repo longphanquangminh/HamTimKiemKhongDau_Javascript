9a5f722c3ea36728aef59c3e2d37d43874113523
Phan Quang Minh Long <lordaizensousuke2001@gmail.com>
2022-08-14 14:18:02 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Tham khảo từ: https://www.tunglt.com/2018/11/bo-dau-tieng-viet-javascript-es6
---
+++ index.js
@@ -0,0 +1,25 @@
+function removeAccents(str) {
+    var AccentsMap = [
+      "aàảãáạăằẳẵắặâầẩẫấậ",
+      "AÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬ",
+      "dđ", "DĐ",
+      "eèẻẽéẹêềểễếệ",
+      "EÈẺẼÉẸÊỀỂỄẾỆ",
+      "iìỉĩíị",
+      "IÌỈĨÍỊ",
+      "oòỏõóọôồổỗốộơờởỡớợ",
+      "OÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢ",
+      "uùủũúụưừửữứự",
+      "UÙỦŨÚỤƯỪỬỮỨỰ",
+      "yỳỷỹýỵ",
+      "YỲỶỸÝỴ"    
+    ];
+    for (var i=0; i<AccentsMap.length; i++) {
+      var re = new RegExp('[' + AccentsMap[i].substr(1) + ']', 'g');
+      var char = AccentsMap[i][0];
+      str = str.replace(re, char);
+    }
+    return str;
+  }
+   
+  console.log(removeAccents("một sản phẩm!"));
